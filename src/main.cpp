0c11a2ab7d61ef47f18793ca000b1faa26bdc6c6
atul sharma <atulsharma406@gmail.com>
2016-05-30 23:37:49 +0530

    made some major changes to every file , main.qml -> item.qml

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <QGuiApplication>
-#include <QtQml>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
     QGuiApplication app(argc, argv);
-   QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
+
+    QQmlApplicationEngine engine;
+    
+    engine.load(QUrl(QStringLiteral("qrc:/item.qml")));
 
     return app.exec();
 }
